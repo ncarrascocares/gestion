082ee9ae2cc47d055fdba41deaa27cb83eab5a3c
Nicolas Carrasco <nicolascarrasco15@gmail.com>
2023-04-25 23:18:52 -0400

    load proyect storage file

---
+++ Config/Config.php
@@ -0,0 +1,8 @@
+<?php
+const BASE_URL = "http://localhost/gestion/";
+const HOST = "localhost";
+const USER = "root";
+const PASS = "";
+const DB = "eventos";
+const CHARSET = "charset=utf8";
+?>
---
+++ Controllers/Principal.php
@@ -0,0 +1,11 @@
+<?php
+class Principal extends Controller
+{
+    public function __construct() {
+        parent::__construct();
+    }
+
+    public function index(){
+        $this->views->getView('principal', 'index');
+    }
+}
